becc4afa9b8b359dd51243d12e30128db9f65b63
shliutai <shliutai@126.com>
2009-08-22 02:50:14 +0000

    board.py -> use ugly1.GIF as background

--- board.py
+++ board.py
@@ -1,25 +1,32 @@
 from Tkinter import *
 from defines import *
 
-class Board(Frame):
-    def __init__(self,parent = None,**config):
-        Frame.__init__(self,parent,config)
-        self.pack(expand =YES,fill=BOTH)
-        self.draw_Map()
-        self.draw_Positions()
-    def draw_Map(self):
-        #Canvas
-        return
-    def draw_Positions(self):
+class Board( Frame ):
+    def __init__( self, parent = None, **config ):
+        Frame.__init__( self, parent, config )
+        self.pack( expand = YES, fill = BOTH )
+        self.Draw_Background()
+        Button( self.back, text = 'button' ).pack()
+    def Draw_Background( self ):
+        self.image = PhotoImage( file = 'ugly1.gif' )
+#        self.wid = self.image.width()
+#        self.hei = self.image.height()
+        self.back = Label( self, image = self.image )
+        self.back.pack( side = TOP, fill = BOTH, expand = YES )
+    def Draw_Map( self, maps ):
         for pos in Pos4:
-            self.drawposition(pos)
+            self.drawposition( pos )
         return
-    def drawposition(self,position,highlight = False):
+    def Draw_Position( self, position, highlight = False ):
         print position
         #draw a rectangle at x y?
         return
-    def draw_Chess(self,somedata):
+    def Draw_Chess( self, somedata ):
         return
+    def run( self ):
+        self.mainloop()
 
-if __name__=='__main__':
-    Board().mainloop()
+if __name__ == '__main__':
+    b = Board()
+#    b.Draw_Map( rule.Map() )
+    b.run()
