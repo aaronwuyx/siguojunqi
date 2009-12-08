82ce3a92e2bb48c837b6ab7f4a786bd15298d925
shliutai <shliutai@126.com>
2009-12-08 13:27:01 +0000

    New msgdefine.py

--- msg.py
+++ msg.py
@@ -115,85 +115,15 @@ class MsgMixin():
             sent = sent + self.sendline( line )
         return sent
 
-    def split( self, target ):
-        if not target:
-            return ( CMD_NONE, None )
-        target = target.strip()
-        target0 = target
-        try:
-            cmd, target = target.split( SEP, 1 )
-        except Exception as e:
-            return ( CMD_ERROR, 'cannot find sep :' + target0 )
-
-        if not ( cmd in msg_cmd ):
-            return ( CMD_ERROR, 'unknown keyword:' + cmd )
-        elif cmd in [CMD_COMMENT, CMD_ASK, CMD_ERROR]:
-            return ( cmd, target )
-        try:
-            typestr, argstr = target.split( SEP, 1 )
-            types = typestr.split( TYPESEP )
-            args = argstr.split( ARGSEP )
-        except:
-            return ( CMD_ERROR, 'cannot find sep :' + target0 )
-        if len( types ) != len( args ):
-            return ( CMD_ERROR, 'len(arg) != len(type) :' + target0 )
-
-        total = len( types )
-        ret = []
-        for count in range( total ):
-            typ = types[count].strip()
-            try:
-                if typ == 'str':
-                    ret.append( args[count] )
-                elif typ == 'int':
-                    ret.append( int( args[count].strip() ) )
-                elif typ == 'float':
-                    ret.append( float( args[count].strip() ) )
-                elif typ == 'lineup':
-                    tmp = define.Lineup( -1 )
-                    tmp.fromStr( args[count] )
-                    ret.append( tmp )
-            except Exception as e:
-                logging.info(str(e))
-                return ( CMD_ERROR, target0 )
-        return ( cmd, ret )
-
     def recv_split( self ):
-        target = self.recvline()
-        return self.split( target )
+        source = self.recvline()
+        return split( source )
 
-    def join( self, cmd, types, arg ):
-        """
-            types, arg's type string
-            arg, a list of values
-        """
-        if cmd in [CMD_COMMENT, CMD_ASK, CMD_ERROR, CMD_NONE]:
-            return cmd + SEP + str( arg )
-        target = cmd + SEP
-        org = ''
-        count = 0
-        for typ in types:
-            if count != 0:
-                target += TYPESEP
-                org += ARGSEP
-            if typ == 'str':
-                org += arg[count]
-            elif typ == 'int':
-                org += str( arg[count] )
-            elif typ == 'float':
-                org += str( arg[count] )
-            elif typ == 'lineup':
-                org += arg[count].toStr()
-            target = target + typ
-            count += 1
-        target += SEP + org + '\n'
-        return target
-
-    def send_join( self, cmd = CMD_NONE, typ = None, arg = None ):
-        target = self.join( cmd, typ, arg )
+    def send_join( self, cmd, typ, arg ):
+        target = join( cmd, typ, arg )
         return self.sendline( target )
 
-    def send_add( self, cmd = CMD_NONE, typ = None, arg = None ):
+    def send_join_add( self, cmd, typ, arg ):
         self.sendstr = self.sendstr + self.join( cmd, typ, arg ) + '\n'
 
 class MsgSocket( MsgMixin, socket.socket ):
@@ -202,6 +132,9 @@ class MsgSocket( MsgMixin, socket.socket ):
         MsgMixin.__init__( self, self )
 
 if __name__ == '__main__':
-    m = MsgSocket()
-    s = m.join( CMD_TELL, ( 'str', 'int', 'float' ), ( 'abcde', 3, 3.0 ) )
+    m1 = MsgSocket()
+    m1.connect( 'localhost', 30000 )
+    #m1.connect( ( 'localhost', 30000 ) )
+    s = join( CMD_TELL, ( 'str', 'int', 'float' ), ( 'abcde', 3, 3.0 ) )
     print( m.split( s ) )
+
